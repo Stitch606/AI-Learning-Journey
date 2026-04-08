ad9af083cd97dbc5e13c90e942255fb437215fc0
Stitch606 <bmhmdalfadl@gmail.com>
2026-04-09 00:01:25 +0200

    Refactor orders system to use dictionary for items
    Refactor order management to use a dictionary for orders and prices. Update user interaction for order input and confirmation.

--- python journey/orders_system.py
+++ python journey/orders_system.py
@@ -1,20 +1,21 @@
-Orders = []
+Orders = {1:"hamburger",2:"Pizza",3:"Hotdog"}
+price_list = {"hamburger":5,"Pizza":12,"Hotdog":4}
+User_order = {}
 print("wellcome")
-print("menu 1.hamburger 2.Pizza 3.Hotdog")
+print("menu: 1.hamburger 2.Pizza 3.Hotdog")
 while True :
-    demand = int(input("Enter your orders:"))
-    match demand :
-        case 1:
-            Orders.append("hamburger")
-        case 2:
-            Orders.append("Pizza")
-        case 3:
-            Orders.append("Hotdog")
-    addition = input("Would you like to add anything else? yes/no :")
-    if addition == "yes" :
+    option_1 = (input("Enter your number orders or q to exit:"))
+    order = Orders.get(option_1)
+    if order :
+        price = price_list.get(order)
+        User_order.update({order:price})
+        option_2 = input("Do you want to continue (y/n):")
+    if "y" :
         continue
-    elif addition == "no" :
-
-        print(f"your orders{Orders}")
-        print("thank you")
-        break
+    elif "n" :
+        print(f"thank you{User_order}your orders")
+        
+        
+         
+    
+    
