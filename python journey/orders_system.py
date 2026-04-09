0ce488c207bbb8cef5030465c3be4bd1df55fec2
Stitch606 <bmhmdalfadl@gmail.com>
2026-04-09 19:04:22 +0200

    Fix input handling and improve order confirmation

--- python journey/orders_system.py
+++ python journey/orders_system.py
@@ -4,18 +4,13 @@ User_order = {}
 print("wellcome")
 print("menu: 1.hamburger 2.Pizza 3.Hotdog")
 while True :
-    option_1 = (input("Enter your number orders or q to exit:"))
+    option_1 = int(input("Enter your number orders :"))
     order = Orders.get(option_1)
     if order :
         price = price_list.get(order)
         User_order.update({order:price})
         option_2 = input("Do you want to continue (y/n):")
-    if "y" :
+    if option_2 == "y" :
         continue
-    elif "n" :
+    elif option_2 ==  "n" :
         print(f"thank you{User_order}your orders")
-        
-        
-         
-    
-    
