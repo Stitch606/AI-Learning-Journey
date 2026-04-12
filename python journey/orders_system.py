249822f1b381cb4749fed4a10913b3f713ad2690
Stitch606 <bmhmdalfadl@gmail.com>
2026-04-12 21:25:22 +0200

    Update orders_system.py
    Added exception handling to manage invalid order inputs and ensure a smoother user experience.

--- python journey/orders_system.py
+++ python journey/orders_system.py
@@ -4,13 +4,19 @@ User_order = {}
 print("wellcome")
 print("menu: 1.hamburger 2.Pizza 3.Hotdog")
 while True :
-    option_1 = int(input("Enter your number orders :"))
-    order = menu.get(option_1)
-    if order :
-        price = price_list.get(order)
-        User_order.update({order:price})
-        option_2 = input("Do you want to continue (y/n):")
-    if option_2 == "y" :
-        continue
-    elif option_2 ==  "n" :
-        print(f"thank you{User_order}your orders")
+    try :
+        option_1 = int(input("Enter your number orders :"))
+        order = menu.get(option_1)
+        if order :
+            price = price_list.get(order)
+            User_order.update({order:price})
+            option_2 = input("Do you want to continue:(y/n)")
+            if option_2 == "y" :
+                continue
+            elif option_2 ==  "n" :
+                print(f"thank you{User_order}your orders")
+                break
+    except :
+        print("incorrect try again")
+total_price = sum(User_order.values())
+print(f"your total price is : {total_price}")
